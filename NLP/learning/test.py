a850640a96b0657b75af883527f3254d45854205
hyunjun yun <kkkths@naver.com>
2021-11-30 23:57:59 +0900

    delete files
    한국어 처리에 필요없는 파일 제거

--- NLP/learning/test.py
+++
@@ -1,4 +0,0 @@
-class Test:
-
-    def test(self):
-        return ""
--- test/processing_test.py
+++
@@ -1,8 +0,0 @@
-# import unittest
-# import NLP.learning.test
-#
-#
-# class ProcessingTest(unittest.TestCase):
-#
-#     def text_cleaning_test(self, input):
-#         proc = Test()
